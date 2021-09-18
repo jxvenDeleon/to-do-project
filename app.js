84cfe763877d9f168070552244b13c1ebd50eb21
Joshua De Leon <>
2021-09-18 01:10:28 -0700

    added express, ejs and started server code

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("testing");
+const express = require('express');
+const app = express()
+
+//set the view engine to ejs for the to do app 
+app.set('view engine','ejs');
+
+
+//-------------------Home Get Request ---------------------//
+app.get('/', (req,res)=>{
+    
+    res.render('index');
+
+});
+
+
+
+
+//------------start the server -------------//
+app.listen(3000, ()=>{
+    console.log("listening on port 3000")
+})
+
